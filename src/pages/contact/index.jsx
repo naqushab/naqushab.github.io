3c04c3059f8f2de2bb321d96656a84feee69fc84
Naqushab Neyazee <neyazee@adobe.com>
2020-07-16 09:27:40 +0530

    update contact me

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import ContentPage from 'components/content-page';
-import Contact from 'components/contact';
-import Meta from 'components/meta';
-import Wrapper from 'components/wrapper';
+import ContentPage from '../../components/content-page';
+import Contact from '../../components/contact';
+import Meta from '../../components/meta';
+import Wrapper from '../../components/wrapper';
 
-const ContactPage = () => (
+const ContactPage = ({ location }) => (
   <ContentPage>
-    <Meta title="Contact" description="If you want to talk about an exciting opportunity or any amazing product, say hello and send an email." />
+    <Meta title="Contact" description="If you have an upcoming project you'd like to talk about or are looking to collaborate and build something special, say hello and send an email." location={location} />
     <Wrapper>
       <Contact />
     </Wrapper>
