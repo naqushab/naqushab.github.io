3fd952ea4ea4aae4a2163891d7bb43c69038d3e4
Naqushab Neyazee <neyazee@adobe.com>
2020-07-16 17:54:56 +0530

    update project index

--- src/components/project/intro/index.jsx
+++ src/components/project/intro/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import OutboundLink from '../../outbound-link';
 import Wrapper from '../../wrapper';
 
+import androidSrc from './img/android.png';
 
 import styles from './styles.module.css';
 
@@ -11,10 +12,12 @@ const ProjectIntro = ({ media, project }) => (
     <div className={styles.intro}>
       <h1>{project.title}</h1>
       <p>{project.clientDescription || project.description}</p>
-      {cta || (
-        <OutboundLink to={project.cta.link}>{project.cta.text}</OutboundLink>
       {project.links.website && <OutboundLink to={project.links.website}>View website</OutboundLink>}
 
+      {project.links.android && (
+        <OutboundLink to={project.links.android} className={styles.download}>
+          <img src={androidSrc} alt="Download on Android" />
+        </OutboundLink>
       )}
     </div>
     <div className={styles.media}>
---
+++ src/data/projects.js
@@ -0,0 +1,60 @@
+const projects = [
+  {
+    className: "adobe",
+    description: "Lead Software Engineer at Adobe, India in the Document Cloud Division aka Adobe Acrobat.",
+    clientDescription:"Working as Lead Software Engineer, I joined Adobe on Jun 27, 2016 in Adobe Acrobat Document Cloud as a Software Engineer.",
+    links: {
+      website: "https://www.adobe.com/in",
+    },
+    slug: "adobe",
+    title: "Adobe",
+    cover: require('./adobe.png'),
+    theme: {
+      background: '#5ab6ff',
+    },
+  },
+
+  {
+    className: 'dabblePoint',
+    description: 'Dabble Point is interactive interview preparation guide which helps users to keep track of the visited questions.',
+    links: {
+      website: 'http://naqushab.pythonanywhere.com/'
+    },
+    slug: 'dabble-point',
+    title: 'Dabble Point',
+    cover: require('./dabblepoint.png'),
+    theme: {
+      background: '#00a5d1',
+    },
+  },
+  {
+    className: 'searchEngineScrapy',
+    description: 'Search Engine Scrapy is a Scrapy project designed to Scrape data from various Search Engines.',
+    clientDescription: "SearchEngineScrapy is a web crawler and scraper for scraping data off various search engines such as Google.com, Bing.com, DuckDuckGo, Ask.com, Baidu. It is based on Python Scrapy project and is developed using Python 2.7 and is compatible with Python 3.",
+    links: {
+      website: 'https://naqushab.github.io/SearchEngineScrapy/',
+    },
+    slug: 'searchEngineScrapy',
+    title: 'Search Engine Scrapy',
+    cover: require('./searchenginescrapy.png'),
+    theme: {
+      background: '#F7F7F7',
+      light: true,
+    },
+  },
+  {
+    className: 'hexApp',
+    description: 'HEX is an android arcade game to control blocks for going out of a hexagon. It is built on ONSENUI platform with the help of monaca.',
+    links: {
+      android: 'http://amzn.com/B00WH4ETNQ"',
+    },
+    slug: 'hex-app',
+    title: 'Hex',
+    cover: require('./hex.jpg'),
+    theme: {
+      background: '#a77e60',
+    },
+  },
+];
+
+export default projects;
