309277cb17829399b9953dd7614022e686401d96
Naqushab Neyazee <neyazee@adobe.com>
2020-07-16 18:21:07 +0530

    update build instructions

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Naqushab Neyazee',
+    url: 'https://naqushab.github.io',
+    description: 'Naqushab Neyazee is Lead Software Engineer at Adobe, India.',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-102826383-1',
+      },
+    },
+  ],
+};
