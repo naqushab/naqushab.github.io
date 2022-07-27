34a34b0a3ebc7be017de89cb894419626778b349
Naqushab Neyazee <naqushabneyazee@zhcet.ac.in>
2022-07-28 04:34:33 +0530

    V2 redesign - republish

---
+++ cmd/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const port = 3000
+
+// For development purposes only.
+func main() {
+	dir := http.Dir("dist")
+	fileServer := http.FileServer(dir)
+	http.Handle("/", fileServer)
+
+	fmt.Printf("Listening on port %d\n", port)
+
+	address := fmt.Sprintf("localhost:%d", port)
+	http.ListenAndServe(address, nil)
+}
